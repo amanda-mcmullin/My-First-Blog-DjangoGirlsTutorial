11c395185cb5539b7b46dd2a1588e3b8f5882894
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-06-27 07:31:12 -0400

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     #assigning a view called post_list to the root URL. This pattern will tell Django that views.post_list is the right place to go if someone enters your website at the 'http://127.0.0.1:8000/' address. The last part, name='post_list', is the name of the URL that will be used to identify the view.
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
