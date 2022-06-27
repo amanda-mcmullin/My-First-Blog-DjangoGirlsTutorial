21a5ab4eaf35f986a450e5135a7e8e086cf74034
Amanda McMullin <amanda.mcmullin@gmail.com>
2022-06-26 21:45:20 -0400

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#function (def) called post_list that takes request and will return the value it gets from calling another function render that will render (put together) our template blog/post_list.html
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), #This line means that for every URL that starts with admin/, Django will find a corresponding view
+    path('', include('blog.urls')),
 ]
